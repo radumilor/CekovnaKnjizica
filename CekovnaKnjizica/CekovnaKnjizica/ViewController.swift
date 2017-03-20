4bacd794bb4332aa777a1fc0ca6601708d09b5c6
Rade Radumilo <rade.radumilo@outlook.com>
2017-03-20 10:40:17 +0000

    New project structure

---
+++ CekovnaKnjizica/CekovnaKnjizica/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CekovnaKnjizica
+//
+//  Created by Rade Radumilo on 3/20/17.
+//  Copyright © 2017 Rade Radumilo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
